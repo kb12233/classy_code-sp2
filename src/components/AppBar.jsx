9ff7aa48231489ef4056137644611f0f89ce4318
Ashley Moriah Lazaraga <ashleylazaraga@gmail.com>
2025-04-07 21:18:28 +0800

    Implement login and register page in  backend

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -25,6 +25,9 @@ import logoDark from '../assets/images/logo_dark.png';
 import AddPhotoAlternate from '@mui/icons-material/AddPhotoAlternate';
 import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 import CodeOutlined from '@mui/icons-material/CodeOutlined';
+import { useAuth } from '../utils/AuthContext';
+import LoadingOverlay from './LoadingOverlay';
+import Sidebar from './SideBar';
 
 const MenuAppBar = forwardRef((props, ref) => {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -35,7 +38,10 @@ const MenuAppBar = forwardRef((props, ref) => {
     const [groupedModels] = useAtom(groupedModelsAtom);
     const [plantUMLCode] = useAtom(plantUmlCodeAtom);
     const [generatedCode] = useAtom(generatedCodeAtom);
+    const [signOutLoading, setSignOutLoading] = useState(false);
     const navigate = useNavigate();
+
+    const { user, logoutUser } = useAuth();
 
     const greencolor = '#B6D9D7';
     const greencolorLight = '#00ffe4'; //changing color of icons
@@ -46,10 +52,16 @@ const MenuAppBar = forwardRef((props, ref) => {
     const handleMenu = (event) => setAnchorEl(event.currentTarget);
     const handleClose = () => setAnchorEl(null);
 
-    const handleSignOut = () => {
-        setAnchorEl(null);
-        navigate('/login');
-    };
+    const handleSignOut = async () => {
+        setSignOutLoading(true);
+        try {
+          await logoutUser();
+          navigate('/login');
+        } catch(error) {
+          console.error("Logout failed:", error.message);
+        }
+        setSignOutLoading(false);
+      };    
 
     const handleModelChange = (event) => {
         setSelectedModel(event.target.value);
@@ -229,6 +241,7 @@ const MenuAppBar = forwardRef((props, ref) => {
                     </Box>
                 </Toolbar>
             </AppBar>
+            <Sidebar isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
         </Box>
     );
 });
