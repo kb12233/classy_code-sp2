f16fdb304bb0c693986e44b1751e421b4fad2043
Ashley Moriah Lazaraga <ashleylazaraga@gmail.com>
2025-04-12 16:09:21 +0800

    Update Register & Login UI

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -38,12 +38,12 @@ export default function LoginPage() {
      {loading && <LoadingOverlay message="Loading..." />}
     <div className="flex h-screen" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
       {/* Left Panel */}
-      <div className="flex flex-col justify-center items-center w-3/5 bg-gray-900 text-white p-10">
+      <div className="flex flex-col justify-center items-center w-3/5 text-white p-10 bg-[#1E1E1E]">
         <img src="\src\assets\images\logo_dark.png" alt="Logo" className="mb-16 w-32" />
-        <h1 className="text-2xl font-bold">Sign in to ClassyCode</h1>
+        <h1 className="text-2xl font-bold ">Sign in to ClassyCode</h1>
         {/* ERROR ALERT */}
         {error && (
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 mt-4 flex items-center justify-between" role="alert">
+            <div className="bg-[#1E1E1E] text-red-700 px-4 py-3 rounded relative mt-4 flex items-center justify-between" role="alert">
             <div className="pr-10">
               <span className="block sm:inline">{error}</span>
             </div>
@@ -51,7 +51,7 @@ export default function LoginPage() {
               onClick={() => setError("")}
               className="absolute top-0 bottom-0 right-0 px-4 py-3"
             >
-              <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <svg className="fill-current h-6 w-6 text-red-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <title>Close</title>
                 <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
               </svg>
@@ -62,32 +62,34 @@ export default function LoginPage() {
           <input
             type="email"
             placeholder="Email"
-            className="w-full p-3 rounded bg-gray-800 text-white border border-gray-700 mb-4"
+            className="w-full p-3 rounded bg-[#303030] text-white border border-[#303030] mb-4"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             placeholder="Password"
-            className="w-full p-3 rounded bg-gray-800 text-white border border-gray-700 mb-4"
+            className="w-full p-3 rounded bg-[#303030] text-white border border-[#303030] mb-4"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           {/* {error && <p className="text-red-500">{error}</p>} */}
-          <p className="text-sm text-teal-400 cursor-pointer mb-4">Forgot your password?</p>
-          <button onClick={handleLogin} className="w-full p-3 bg-teal-600 hover:bg-teal-700 text-white rounded-lg transition">
+          <p className="text-[#B4B4B4] cursor-pointer mb-4">Forgot your password?</p>
+          <button onClick={handleLogin} className="w-full p-3 bg-[#212121] hover:bg-[#B4B4B4] 
+            hover:text-[#303030] text-[#B4B4B4] rounded-lg transition border border-[#303030]">
             SIGN IN
           </button>
         </div>
       </div>
 
       {/* Right Panel */}
-      <div className="flex flex-col justify-center items-center w-2/5 bg-[#b8dbd9] p-10">
-        <h2 className="text-2xl font-bold text-gray-900">Hello Friend!</h2>
-        <p className="text-center text-gray-800 mt-4 px-10">
+      <div className="flex flex-col justify-center items-center w-2/5 p-10 bg-[#B4B4B4]">
+        <h2 className="text-2xl font-bold text-[#303030]">Hello Friend!</h2>
+        <p className="text-center mt-4 px-10 text-[#303030]">
           Create an account and get started!
         </p>
-        <button onClick={handleRegister} className="mt-6 px-6 py-2 border border-gray-900 text-gray-900 rounded hover:bg-gray-900 hover:text-white">
+        <button onClick={handleRegister} className="mt-6 px-6 py-2 border border-gray-900 
+          text-[#212121] rounded-lg hover:bg-[#303030] hover:text-[#B4B4B4]">
           SIGN UP
         </button>
       </div>
--- src/components/RegistrationPage.jsx
+++ src/components/RegistrationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../utils/AuthContext';
 import LoadingOverlay from '../components/LoadingOverlay';
@@ -39,33 +39,34 @@ const RegistrationPage = () => {
         {loading && <LoadingOverlay message="Loading..." />}
         <div className="flex h-screen w-screen" style={{ fontFamily: 'JetBrains Mono, monospace' }}>
             {/* Left Section */}
-            <div className="flex flex-col items-center justify-center flex-2 bg-[#b8dbd9] p-10">
+            <div className="flex flex-col items-center justify-center flex-2 bg-[#B4B4B4] p-10">
                 <img src="\src\assets\images\logo_light.png" alt="Logo" className="w-32 h-32" />
-                <h1 className="text-gray-800 text-2xl font-bold mt-20">Welcome Back!</h1>
-                <p className="text-gray-700 text-sm mt-4 text-center w-2/3">
+                <h1 className="text-[#212121] text-2xl font-bold mt-20">Welcome Back!</h1>
+                <p className="text-[#212121] text-sm mt-4 text-center w-2/3">
                     Stay in touch! Sign in with your info.
                 </p>
-                <button onClick={handleLogin} className="mt-6 px-6 py-2 border border-gray-800 text-gray-800 rounded-lg hover:bg-gray-800 hover:text-white transition">
+                <button onClick={handleLogin} className="mt-6 px-6 py-2 border border-gray-900 
+                    text-[#212121] rounded-lg hover:bg-[#303030] hover:text-[#B4B4B4]">
                     SIGN IN
                 </button>
             </div>
             {/* Right Section */}
-            <div className="flex flex-col items-center justify-center flex-3 bg-gray-900 text-white p-10">
+            <div className="flex flex-col items-center justify-center flex-3 bg-[#1E1E1E] text-white p-10">
                 <h2 className="text-3xl font-bold">Create Account</h2>
 
                 {error && (
-                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4 mt-4 flex items-center justify-between" role="alert">
+                    <div className="bg-[#1E1E1E] text-red-700 px-4 py-3 rounded relative mt-4 flex items-center justify-between" role="alert">
                     <div className="pr-10">
-                        <span className="block sm:inline">{error}</span>
+                      <span className="block sm:inline">{error}</span>
                     </div>
                     <button
-                        onClick={() => setError("")}
-                        className="absolute top-0 bottom-0 right-0 px-4 py-3"
+                      onClick={() => setError("")}
+                      className="absolute top-0 bottom-0 right-0 px-4 py-3"
                     >
-                        <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <title>Close</title>
-                            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
-                        </svg>
+                      <svg className="fill-current h-6 w-6 text-red-700" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                      </svg>
                     </button>
                 </div>
                 )}
@@ -76,12 +77,12 @@ const RegistrationPage = () => {
                     </div>
                 )}
 
-                <div className="mt-10 w-1/3">
+                <div className="mt-5 w-1/2">
                     <div className="mb-4">
                         <label className="block text-sm mb-1">Name</label>
                         <input
                             type="text"
-                            className="w-full p-2 border border-gray-700 rounded bg-gray-800 text-white"
+                            className="w-full p-2 border border-[#303030] rounded bg-[#303030] text-white"
                             placeholder="Enter your name"
                             value={name}
                             onChange={(e) => setName(e.target.value)}
@@ -91,7 +92,7 @@ const RegistrationPage = () => {
                         <label className="block text-sm mb-1">Email</label>
                         <input
                             type="email"
-                            className="w-full p-2 border border-gray-700 rounded bg-gray-800 text-white"
+                            className="w-full p-2 border border-[#303030] rounded bg-[#303030] text-white"
                             placeholder="Enter your email"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
@@ -101,13 +102,14 @@ const RegistrationPage = () => {
                         <label className="block text-sm mb-1">Password</label>
                         <input
                             type="password"
-                            className="w-full p-2 border border-gray-700 rounded bg-gray-800 text-white"
+                            className="w-full p-2 border border-[#303030] rounded bg-[#303030] text-white"
                             placeholder="Enter your password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
                         />
                     </div>
-                    <button onClick={handleRegister} className="w-full mt-4 bg-teal-600 hover:bg-teal-700 text-white py-2 rounded-lg transition">
+                    <button onClick={handleRegister} className="w-full mt-4 py-2 bg-[#212121] hover:bg-[#B4B4B4] 
+                        hover:text-[#303030] text-[#B4B4B4] rounded-lg transition border border-[#303030]">
                         SIGN UP
                     </button>
                 </div>
